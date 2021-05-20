ea3e5696d60cc07de19383cf3a46e0f4df2a9636
Sunny <sunnyshh45@gmail.com>
2021-05-19 20:00:44 -0400

    implement required service, models

---
+++ video-game-db/src/app/model/game.model.ts
@@ -0,0 +1,47 @@
+import { Rating } from "src/app/model/rating.model";
+
+export interface Game {
+  background_image: string;
+  name: string;
+  released: string;
+  metacritic_url: string;
+  website: string;
+  description: string;
+  metacritic: number;
+  genres: Array<Genre>;
+  parent_platforms: Array<ParentPlatform>;
+  publishers: Array<Publishers>;
+  ratings: Array<Rating>;
+  screenshots: Array<Screenshots>;
+  trailers: Array<Trailer>;
+}
+
+// whatever will be passed to this interface, will get in forms of an array
+export interface APIResponse<T> {
+  results: Array<T>;
+}
+
+
+interface Genre {
+  name: string;
+}
+
+interface ParentPlatform {
+  platform: {
+    name: string;
+  };
+}
+
+interface Publishers {
+  name: string;
+}
+
+interface Screenshots {
+  image: string;
+}
+
+interface Trailer {
+  data: {
+    max: string;
+  };
+}
